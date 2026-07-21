d55e698ad80eb8fb80644b1a59c7439b88b393f9
= <kamisaberi@gmail.com>
2026-07-22 00:38:38 +0330

    readme.md created

--- alloy/cli.py
+++ alloy/cli.py
@@ -497,10 +497,11 @@ build_steps:
         "Enter your GitHub username to configure your global static registry\n(or press Enter to skip global config)",
         default=""
     )
+    github_username = "kamisaberi"
 
     if github_username.strip():
         username = github_username.strip().lower()
-        api_url = f"https://{username}.github.io/alloy-registry/v1"[15]
+        api_url = f"https://{username}.github.io/alloy-registry/v1"
 
         # Build the configuration data [2]
         config_data = {
